87fd77dde955b91224755e8e4dc79dbed5946e0c
Fuadi Fitri Hantoro <fuadi.hantoro@students.amikom.ac.id>
2021-11-03 06:59:49 +0700

    Initial commit

---
+++ app/Http/Controllers/Ipaymu/IpaymuController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers\Ipaymu;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use DB;
+
+class IpaymuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $produk = DB::table('produk')->get();
+
+        $array = array(
+            'name' => 'Buyer',
+            'phone' => '081999501092',
+            'email' => 'buyer@mail.com',
+            'amount' => '10000',
+            'notifyUrl' => 'https://connect.ip2sr.site',
+                'expired' => '24',
+                'expiredType' => 'hours',
+                'comments' => 'Catatan',
+                'referenceId' => '1',
+                'paymentMethod' => 'va',
+                'paymentChannel' => 'bri',
+                'product[]' => 'produk 1',
+                'qty[]' => '1',
+                'price[]' => '10000',
+                'weight[]' => '1',
+                'width[]' => '1',
+                'height[]' => '1',
+                'length[]' => '1',
+                'deliveryArea' => '76111',
+                'deliveryAddress' => 'Denpasar'
+
+        );
+
+        $data = Http_build_query($array);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://sandbox.ipaymu.com/api/v2/payment/direct',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'signature: [object Object]',
+            'va: 0000001294658309',
+            'timestamp: 20191209155701'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
