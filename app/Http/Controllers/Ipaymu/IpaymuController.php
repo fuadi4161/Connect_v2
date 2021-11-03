281600af6163292c5f59ee17b73a5c3f5822d727
Fuadi Fitri Hantoro <fuadi.hantoro@students.amikom.ac.id>
2021-11-03 08:43:02 +0700

    Update IpaymuController.php

--- app/Http/Controllers/Ipaymu/IpaymuController.php
+++ app/Http/Controllers/Ipaymu/IpaymuController.php
@@ -18,49 +18,32 @@ class IpaymuController extends Controller
     public function index()
     {
 
-        require_once 'HTTP/Request2.php';
-        $request = new HTTP_Request2();
-        $request->setUrl('https://sandbox.ipaymu.com/api/v2/payment');
-        $request->setMethod(HTTP_Request2::METHOD_POST);
-        $request->setConfig(array(
-        'follow_redirects' => TRUE
-        ));
-        $request->setHeader(array(
-        'Content-Type' => 'application/json',
-        'signature' => '[object Object]',
-        'va' => '0000001294658309',
-        'timestamp' => '20191209155701'
-        ));
-        $request->addPostParameter(array(
-        'product[]' => 'Baju',
-        'qty[]' => '1',
-        'price[]' => '10000',
-        'description[]' => 'Baju1',
-        'returnUrl' => 'https://ipaymu.com/return',
-        'notifyUrl' => 'https://ipaymu.com/notify',
-        'cancelUrl' => 'https://ipaymu.com/cancel',
-        'referenceId' => 'ID1234',
-        'weight[]' => '1',
-        'dimension[]' => '1:1:1',
-        'buyerName' => 'putu',
-        'buyerEmail' => 'putu@mail.com',
-        'buyerPhone' => '08123456789',
-        'pickupArea' => '80117',
-        'pickupAddress' => 'Jakarta'
-        ));
-        try {
-        $response = $request->send();
-        if ($response->getStatus() == 200) {
-            echo $response->getBody();
-        }
-        else {
-            echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
-            $response->getReasonPhrase();
-        }
-        }
-        catch(HTTP_Request2_Exception $e) {
-        echo 'Error: ' . $e->getMessage();
-        }
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'signature' => '[object Object]',
+            'va' => '0000001294658309',
+            'timestamp' => '20191209155701'
+        ])->post('https://sandbox.ipaymu.com/api/v2/payment', [
+            'product[]' => 'Baju',
+            'qty[]' => '1',
+            'price[]' => '10000',
+            'description[]' => 'Baju1',
+            'returnUrl' => 'https://ipaymu.com/return',
+            'notifyUrl' => 'https://ipaymu.com/notify',
+            'cancelUrl' => 'https://ipaymu.com/cancel',
+            'referenceId' => 'ID1234',
+            'weight[]' => '1',
+            'dimension[]' => '1:1:1',
+            'buyerName' => 'putu',
+            'buyerEmail' => 'putu@mail.com',
+            'buyerPhone' => '08123456789',
+            'pickupArea' => '80117',
+            'pickupAddress' => 'Jakarta',
+        ]);
+
+        print $response;
+
+        
                 
     }
     /**
