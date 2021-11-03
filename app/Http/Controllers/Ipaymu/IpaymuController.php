df9a4f3a146c655c44edde8be6c5e36f648060ce
Fuadi Fitri Hantoro <fuadi.hantoro@students.amikom.ac.id>
2021-11-03 11:29:44 +0700

    Update IpaymuController.php

--- app/Http/Controllers/Ipaymu/IpaymuController.php
+++ app/Http/Controllers/Ipaymu/IpaymuController.php
@@ -18,30 +18,70 @@ class IpaymuController extends Controller
     public function index()
     {
 
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'signature' => '[object Object]',
-            'va' => '1179000899',
-            'timestamp' => '20191209155701'
-        ])->post('https://sandbox.ipaymu.com/api/v2/payment', [
-            'product[]' => 'Baju',
-            'qty[]' => '1',
-            'price[]' => '10000',
-            'description[]' => 'Baju1',
-            'returnUrl' => 'https://ipaymu.com/return',
-            'notifyUrl' => 'https://ipaymu.com/notify',
-            'cancelUrl' => 'https://ipaymu.com/cancel',
-            'referenceId' => 'ID1234',
-            'weight[]' => '1',
-            'dimension[]' => '1:1:1',
-            'buyerName' => 'putu',
-            'buyerEmail' => 'putu@mail.com',
-            'buyerPhone' => '08123456789',
-            'pickupArea' => '80117',
-            'pickupAddress' => 'Jakarta',
-        ]);
+         // SAMPLE HIT API iPaymu v2 PHP //
 
-        print $response;
+        $va           = 'yout_va'; //get on iPaymu dashboard
+        $secret       = 'yout_api_key'; //get on iPaymu dashboard
+
+        $url          = 'https://my.ipaymu.com/api/v2/payment'; //url
+        $method       = 'POST'; //method
+
+        //Request Body//
+        $body['product']    = array('headset', 'softcase');
+        $body['qty']        = array('1', '3');
+        $body['price']      = array('100000', '20000');
+        $body['returnUrl']  = 'https://mywebsite.com/thankyou';
+        $body['cancelUrl']  = 'https://mywebsite.com/cancel';
+        $body['notifyUrl']  = 'https://mywebsite.com/notify';
+        //End Request Body//
+
+        //Generate Signature
+        // *Don't change this
+        $jsonBody     = json_encode($body, JSON_UNESCAPED_SLASHES);
+        $requestBody  = strtolower(hash('sha256', $jsonBody));
+        $stringToSign = strtoupper($method) . ':' . $va . ':' . $requestBody . ':' . $secret;
+        $signature    = hash_hmac('sha256', $stringToSign, $secret);
+        $timestamp    = Date('YmdHis');
+        //End Generate Signature
+
+
+        $ch = curl_init($url);
+
+        $headers = array(
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'va: ' . $va,
+            'signature: ' . $signature,
+            'timestamp: ' . $timestamp
+        );
+
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        curl_setopt($ch, CURLOPT_POST, count($body));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonBody);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $err = curl_error($ch);
+        $ret = curl_exec($ch);
+        curl_close($ch);
+        if($err) {
+            echo $err;
+        } else {
+
+            //Response
+            $ret = json_decode($ret);
+            if($ret->Status == 200) {
+                $sessionId  = $ret->Data->SessionID;
+                $url        =  $ret->Data->Url;
+                header('Location:' . $url);
+            } else {
+                echo $ret;
+            }
+            //End Response
+        }
 
         
                 
