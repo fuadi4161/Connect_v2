b39d75108e5bf13859b9f30ee71e4cd0393e8e49
Fuadi Fitri Hantoro <fuadi.hantoro@students.amikom.ac.id>
2021-12-12 10:42:40 +0700

    Update ApiPaymentController.php

--- app/Http/Controllers/ApiController/ApiPaymentController.php
+++ app/Http/Controllers/ApiController/ApiPaymentController.php
@@ -112,21 +112,20 @@ class ApiPaymentController extends Controller
                 if ($cek->isEmpty()) {
                     
 
-                    $nominal = DB::table('users')->where('users.id', $userid)
+                    $users = DB::table('users')->where('users.id', $request->userid)
                             ->leftJoin('client', 'users.id', '=', 'client.id_user')
-                            ->select('client.nominal','users.*')
+                            ->select('users.*', 'client.nominal')
                             ->get();
 
-                            return response()->json($nominal);
-                        // foreach ($nominal as $detail) {
-                        //     $items = $detail->nominal;
-                        //     $name = $detail->name;
-                        // }
+
+                    foreach($users as $id){
+                        $datausers = $id;
+                    }
 
                     // membuat input pembayaran ketika users tergenerate lunas
                     DB::table('pembayaran')->insert([
                         'user_id' => $userid,
-                        'nominal' =>  $items,
+                        'nominal' => $datausers->nominal,
                         'bulan' => Carbon::now()->isoformat('MMMM'),
                         'tahun' => date('Y'),
                         'author_id' => Auth::user()->id,
@@ -157,7 +156,7 @@ class ApiPaymentController extends Controller
                     DB::table('aktivitas')->insert([
                         'user_id' => Auth::user()->id,
                         'judul' => 'Konfirmasi Iuran',
-                        'deskripsi' => 'Admin Telah mengkonfirmasi iuran'+ $name,
+                        'deskripsi' => Auth::user()->name +' Telah mengkonfirmasi iuran'+ $datausers->name,
                         'created_at' => date('d-m-Y H:i:s'),
                         'updated_at' => date('d-m-Y H:i:s'),
                     ]);
@@ -245,7 +244,7 @@ class ApiPaymentController extends Controller
                     DB::table('aktivitas')->insert([
                         'user_id' => Auth::user()->id,
                         'judul' => 'Konfirmasi Iuran',
-                        'deskripsi' => 'Admin Telah mengkonfirmasi iuran '+ $name,
+                        'deskripsi' => 'Admin Telah mengkonfirmasi iuran '+ $datausers->name,
                         'created_at' => date('d-m-Y H:i:s'),
                         'updated_at' => date('d-m-Y H:i:s'),
                     ]);
@@ -314,10 +313,10 @@ class ApiPaymentController extends Controller
             }
 
 
-        // return response()->json([
-        //     'success' => true,
-        //     'pesan' => 'Berhasil update data'
-        // ],201);
+        return response()->json([
+            'success' => true,
+            'pesan' => 'Berhasil update data'
+        ],201);
         
     }
 
